4948f9759582da10f10f0ca2812e33907e5cf3f8
yoge-esh <yogesh124khanal@gmail.com>
2022-01-21 21:09:37 +0545

    updated with react routes

--- start/src/App.js
+++ start/src/App.js
@@ -2,36 +2,56 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import TextForms from "./Components/TextForms";
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); // state variable to ckeck the dark mode enable or not to check from, here 
+  const [mode, setMode] = useState("light"); // state variable to ckeck the dark mode enable or not to check from, here
 
-  // making an switch case to enable dark and light mode 
+  // making an switch case to enable dark and light mode
   const toggleMode = () => {
-    if (mode === 'light'){
-      setMode('dark')
-      document.body.style.backgroundColor = 'grey';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
       // document.body.style.textColor = 'white';
-    }
-    else{
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       // document.body.style.textColor = 'dark';
     }
-  }
+  };
+
   return (
     <>
-      <Navbar title="Start Project" aboutText="About Us" mode = {mode} toggleMode = {toggleMode}/>
+    <Router>
+      <Navbar
+        title="Start Project"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
 
       <div className="container my-3">
-        <TextForms heading="Enter a text to Analyze" mode = {mode}/>
+          <Switch>
+            {/* home page */}
+            <Route exact path="/">
+              <TextForms heading="Enter a text to Analyze" mode={mode} />
+            </Route>
+
+            {/* about page */}
+            <Route exact path="/about">
+              <About />
+            </Route>
+            {/* <Route path="/:user" component={User} /> */}
+
+            {/* no mathch */}
+            {/* <Route component={NoMatch} /> */}
+
+          </Switch>
 
         {/* <TextForms/> */}
-
-        {/* <About/> */}
       </div>
-
+        </Router>
     </>
   );
 }
