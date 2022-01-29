bd42ef1f7e6a5c2913c8e32c0cae718c5c3a03bf
yoge-esh <yogesh124khanal@gmail.com>
2022-01-29 20:27:35 +0545

    updates of forms

--- livecoronatracker/src/App.js
+++ livecoronatracker/src/App.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setname] = useState("");
 
-  const bgChnage = () =>{
-    console.log('clicke!')
-  }
-  return(
-  <div style={ {backgroundColor: "crimsion"} }>
-      <h2>Welcome to Events in React Js </h2>
-      <button onClick={bgChnage}>Click me</button>
-  </div>
+  //   shows user input full name after hello
+  //   const [firstName, setfirstName] = useState('');
+
+  //   const [lastName, setLastName] = useState('');
+
+  //   const [LastName, setlastName] = useState('');
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setname(event.target.value);
+  };
+
+  const submit = (event) => {
+    event.preventDefault(); //prevent default action of form submit which is on page refresh
+    alert('form submmitted');
+
+    // setfirstName(name);
+    // setlastName(lastName);
+  };
+
+  //   const inputEventTwo = (event) => {
+  //     setLastName(event.target.value);
+
+  //   };
+
+  return (
+    <>
+      <div>
+        <h2>Welcome to React Form</h2>
+        <form onSubmit={submit}>
+          <input
+            type="text"
+            placeholder="Enter your First Name..."
+            onChange={inputEvent}
+            value = {fname}
+          />
+          <input
+            type="text"
+            placeholder="Enter your Last Name..."
+            onChange={inputEvent}
+            value={lname}
+          />
+          {/* value={name} is used to display the value of the input  which is also called as a single source of input*/}
+
+          <button type="submit">Click Me</button>
+        </form>
+      </div>
+    </>
   );
 }
 
